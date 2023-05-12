36fa66a34f67ce62b49a5ea03f4f01fbe1209ae0
ageorge95 <arteni.george.daniel@gmail.com>
2023-05-12 20:00:30 +0300

    added a new type hint and tests in decimals_places()

--- DecimalScripts/decimals_places.py
+++ DecimalScripts/decimals_places.py
@@ -1,6 +1,6 @@
 from decimal import Decimal
 
-def decimals_places(nr: str | float | int) -> int:
+def decimals_places(nr: Decimal | str | float | int) -> int:
 
     nr = str(nr)
 
@@ -22,7 +22,9 @@ if __name__ == '__main__':
         ['1.1', 1], ['1.114', 3], ['-4.56', 2],
         [1.10, 1], [1.11400, 3], [-4.56000, 2],
         ['1.10', 1], ['1.11400', 3], ['-4.56000', 2],
-        ['.1', 1], ['.1456', 4]
+        ['.1', 1], ['.1456', 4],
+        [Decimal('1.10'), 1], [Decimal('1.11400'), 3], [Decimal('-4.56000'), 2],
+        [Decimal('.1'), 1], [Decimal('.1456'), 4]
     ]:
         result = decimals_places(test[0])
         assert result == test[1] ,\
