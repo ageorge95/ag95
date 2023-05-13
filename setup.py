218abf2a759d74520c1d3e7e44adeee2c96eb470
ageorge95 <arteni.george.daniel@gmail.com>
2023-05-13 19:09:20 +0300

    refactored the folder structure to allow easy pip install directly from git

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+VERSION = '0.0.0'
+DESCRIPTION = 'ag95 scripts collection'
+LONG_DESCRIPTION = 'Various scripts that can be used in all kinds of python projects.'
+
+setup(
+    name="ag95",
+    version=VERSION,
+    author="ageorge95",
+    # author_email="some_email@provider.com",
+    description=DESCRIPTION,
+    long_description=LONG_DESCRIPTION,
+    packages=find_packages(),
+    install_requires=[],  # add any additional packages that
+    # needs to be installed along with your package. Eg: 'caer'
+    keywords=['python', 'ag95']
+)
