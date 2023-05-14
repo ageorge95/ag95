b6ddb4ade770affe854e39463a0756837f6db71c
ageorge95 <arteni.george.daniel@gmail.com>
2023-05-14 08:26:11 +0300

    added the missing configure_logger import in __init__.py

--- ag95/__init__.py
+++ ag95/__init__.py
@@ -2,3 +2,4 @@ from .DecimalScripts.decimals_places import decimals_places
 from .DecimalScripts.nr_normalisation import nr_normalisation
 from .DecimalScripts.round_closer import round_down_closer,\
     round_up_closer
+from .LoggingScripts.custom_logger import configure_logger
