64316c5326fed31731a1c0d9644659a589563a17
ageorge95 <arteni.george.daniel@gmail.com>
2023-05-14 08:23:59 +0300

    added LoggingScripts/custom_logger.py

---
+++ ag95/LoggingScripts/custom_logger.py
@@ -0,0 +1,67 @@
+from logging import basicConfig,\
+    INFO, DEBUG, WARNING, ERROR, CRITICAL,\
+    Formatter,\
+    StreamHandler
+from concurrent_log_handler import ConcurrentRotatingFileHandler
+from sys import stdout,\
+    platform
+from os import system
+from typing import Literal
+
+def configure_logger(log_name : str = "runtime_log.log",
+                     maxBytes: int = 20*1024*1024,
+                     backupCount: int = 2,
+                     log_level: Literal['DEBUG', 'INFO', 'WARNING', 'ERROR', 'CRITICAL'] = DEBUG):
+
+    class CustomFormatter(Formatter):
+        grey = "\x1b[38;21m"
+        yellow = "\x1b[33;21m"
+        red = "\x1b[31;21m"
+        bold_red = "\x1b[31;1m"
+        reset = "\x1b[0m"
+        format = '%(asctime)s,%(msecs)d %(levelname)-4s [%(filename)s:%(lineno)d -> %(name)s - %(funcName)s] ___ %(message)s'
+
+        FORMATS = {
+            DEBUG: grey + format + reset,
+            INFO: grey + format + reset,
+            WARNING: yellow + format + reset,
+            ERROR: red + format + reset,
+            CRITICAL: bold_red + format + reset
+        }
+
+        def format(self, record):
+            log_fmt = self.FORMATS.get(record.levelno)
+            formatter = Formatter(log_fmt)
+            return formatter.format(record)
+
+    if platform == 'win32':
+        # enable colored console text on windows
+        system('color')
+
+    ch = StreamHandler(stream=stdout)
+    ch.setLevel(DEBUG)
+    ch.setFormatter(CustomFormatter())
+    fh = ConcurrentRotatingFileHandler(log_name,
+                                       mode='a',
+                                       maxBytes=maxBytes,
+                                       backupCount=backupCount)
+    fh.setLevel(DEBUG)
+    fh.setFormatter(Formatter('%(asctime)s,%(msecs)d %(levelname)-4s [%(filename)s:%(lineno)d -> %(name)s - %(funcName)s] ___ %(message)s'))
+
+    basicConfig(datefmt='%Y-%m-%d:%H:%M:%S',
+                level=log_level,
+                handlers=[fh,ch])
+
+if __name__ == '__main__':
+    from logging import getLogger
+
+    configure_logger()
+    log = getLogger()
+
+    log.debug('I am a DEBUG message')
+    log.info('I am an INFO message')
+    log.warning('I am a WARNING message')
+    log.error('I am an ERROR message')
+    log.critical('I am a CRITICAL message')
+
+    print('MANUAL assessment required !')
--- setup.py
+++ setup.py
@@ -12,7 +12,9 @@ setup(
     description=DESCRIPTION,
     long_description=LONG_DESCRIPTION,
     packages=find_packages(),
-    install_requires=[],  # add any additional packages that
+    install_requires=[
+        'concurrent_log_handler',
+    ],  # add any additional packages that
     # needs to be installed along with your package. Eg: 'caer'
     keywords=['python', 'ag95']
 )
