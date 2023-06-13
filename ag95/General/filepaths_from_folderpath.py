c0a948e16bc29759e62e9fcaf7991250f97722d1
ageorge95 <arteni.george.daniel@gmail.com>
2023-06-13 14:27:04 +0300

    added a new useful script - filepaths_from_folderpath.py

---
+++ ag95/General/filepaths_from_folderpath.py
@@ -0,0 +1,25 @@
+from typing import AnyStr, List
+from os import path,\
+    walk
+from logging import getLogger
+
+def extract_filenames_from_filepath(folderpath: AnyStr,
+                                    filter_ends_with: List[AnyStr] = None) -> List:
+    _log = getLogger()
+    filepaths = []
+
+    _log.info(f'Analysing {folderpath} ... with the following filters: {filter}')
+    if path.isdir(folderpath):
+        for first_path, subdirs, files in walk(folderpath):
+            for name in files:
+                if (not filter_ends_with) or\
+                        (filter_ends_with and any([name.endswith(_) for _ in filter_ends_with])):
+                    filepaths.append(path.join(first_path, name))
+        _log.info(f'Gathered {len(filepaths)} TOTAL recordings from {folderpath}')
+
+    return filepaths
+
+if __name__ == '__main__':
+    from pprint import pprint
+    pprint((extract_filenames_from_filepath(folderpath='.')))
+    print('No automatic tests implemented so far; Please check the expected behavior manually.')
--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, find_packages
 
-VERSION = '0.2.0'
+VERSION = '0.2.1'
 DESCRIPTION = 'ag95 scripts collection'
 LONG_DESCRIPTION = 'Various scripts that can be used in all kinds of python projects.'
 
