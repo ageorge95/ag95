914a5cbbe78c9bb277506f69e3d22f230c005742
ageorge95 <arteni.george.daniel@gmail.com>
2024-09-06 12:54:39 +0300

    expanded the test suite for nr_normalisation

--- ag95/DecimalScripts/nr_normalisation.py
+++ ag95/DecimalScripts/nr_normalisation.py
@@ -1,13 +1,15 @@
 from decimal import Decimal
+from itertools import count
+
 
 def nr_normalisation(nr: Decimal | str | int | float,
                      decimals_overwrite: int = None) -> str:
 
     def remove_invalid_trailing(data):
-        # remove all trailing 0s and dots (.) or commas (,)
-        while data.endswith('0') and len(data) > 1 and (',' in data or '.' in data):
+        # remove all trailing decimal 0s and dots (.)
+        while data.endswith('0') and len(data) > 1 and '.' in data:
             data = data[:-1]
-        if data.endswith('.') or data.endswith(','):
+        if data.endswith('.'):
             data = data[:-1]
         return data
 
@@ -15,18 +17,24 @@ def nr_normalisation(nr: Decimal | str | int | float,
     if not isinstance(nr, str):
         nr = str(nr)
 
+    # replace commas (,) with dot (.)
+    nr = nr.replace(',', '.')
     nr = remove_invalid_trailing(nr)
 
-    # figure out the number of decimals
-    if '.' not in nr and ',' not in nr:
-        nr_decimal_places = 0
-    elif '.' in nr and ',' not in nr:
-        nr_decimal_places = len(nr.split('.')[1])
-    elif ',' in nr and '.' not in nr:
-        nr_decimal_places = len(nr.split(',')[1])
-    else:
+    # check if the number is vlaid for further processing
+    if nr.count('.') > 1:
         print('Invalid number provided !')
         return None
+
+    # remove the non-scientific representation
+    if not isinstance(nr, Decimal):
+        nr = str(Decimal(nr))
+
+    # figure out the number of decimals
+    if '.' not in nr:
+        nr_decimal_places = 0
+    elif '.' in nr:
+        nr_decimal_places = len(nr.split('.')[1])
 
     # truncate the decimals if requested
     if nr_decimal_places: # but only if there are decimals
@@ -70,8 +78,8 @@ if __name__ == '__main__':
         [['34.2345', 0], '34'], [['43234.23231450000', 0], '43234'],
         [['34.5345', 0], '34'], [['43234.73231450000', 0], '43234'],
 
-        [['34,2345', 2], '34,23'],
-        [['34,2', 1], '34,2'],
+        [['34,2345', 2], '34.23'],
+        [['34,2', 1], '34.2'],
         [['34,', 1], '34'], [[34, 5], '34'],
         [['34,0', 1], '34'], [[34.0, 5], '34'],
         [['34,2345', 0], '34'], [['43234,23231450000', 0], '43234'],
@@ -79,6 +87,11 @@ if __name__ == '__main__':
 
         [['34,2345.7', 2], None],
         [['34,.2', 1], None],
+
+        [['7.02e-06'], '0.00000702'],
+        [['7.02e-06', 7], '0.000007'],
+        [['7.02e-06', 8], '0.00000702'],
+        [['7.02e-06', 2], '0'],
     ]:
         result = nr_normalisation(*test[0])
         assert result == test[1] ,\
