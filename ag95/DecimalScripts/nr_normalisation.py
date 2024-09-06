16983bfde15faa74b9659c841b76216449d7eda3
ageorge95 <arteni.george.daniel@gmail.com>
2024-09-06 12:42:33 +0300

    rewritten large parts of the nr_normalisation logic

--- ag95/DecimalScripts/nr_normalisation.py
+++ ag95/DecimalScripts/nr_normalisation.py
@@ -2,26 +2,49 @@ from decimal import Decimal
 
 def nr_normalisation(nr: Decimal | str | int | float,
                      decimals_overwrite: int = None) -> str:
-    nr = str(nr)
 
-    # first remove any trailing of 0 from the nr
-    # nr must first be converted to a Decimal, to correctly display it
-    if type(nr) != type(Decimal):
-        nr = Decimal(str(nr))
+    def remove_invalid_trailing(data):
+        # remove all trailing 0s and dots (.) or commas (,)
+        while data.endswith('0') and len(data) > 1 and (',' in data or '.' in data):
+            data = data[:-1]
+        if data.endswith('.') or data.endswith(','):
+            data = data[:-1]
+        return data
 
-    nr_decimal_places = abs(nr.as_tuple().exponent)
-    nr = f'{nr:.{nr_decimal_places if decimals_overwrite is None else decimals_overwrite}f}'
+    # convert the number to string for processing
+    if not isinstance(nr, str):
+        nr = str(nr)
 
-    # now remove all trailing 0s
-    while nr.endswith('0') and len(nr) > 1 and (',' in nr or '.' in nr):
-        nr = nr[:-1]
+    nr = remove_invalid_trailing(nr)
 
-    # then convert the number to a decimal
-    nr = Decimal(str(nr))
+    # figure out the number of decimals
+    if '.' not in nr and ',' not in nr:
+        nr_decimal_places = 0
+    elif '.' in nr and ',' not in nr:
+        nr_decimal_places = len(nr.split('.')[1])
+    elif ',' in nr and '.' not in nr:
+        nr_decimal_places = len(nr.split(',')[1])
+    else:
+        print('Invalid number provided !')
+        return None
 
-    # and finally return it
-    nr_decimal_places = abs(nr.as_tuple().exponent)
-    return f'{nr:.{nr_decimal_places}f}'
+    # truncate the decimals if requested
+    if nr_decimal_places: # but only if there are decimals
+
+        if '.' in nr:
+            nr, decimals = nr.split('.')
+            separator = '.'
+        elif ',' in nr:
+            nr, decimals = nr.split(',')
+            separator = ','
+
+        decimals = decimals[:decimals_overwrite]
+
+        nr = nr+separator+decimals
+
+    nr = remove_invalid_trailing(nr)
+
+    return nr
 
 if __name__ == '__main__':
 
@@ -31,26 +54,36 @@ if __name__ == '__main__':
         [[34, 1], '34'], [[34, 5], '34'],
         [[34.0, 1], '34'], [[34.0, 5], '34'],
         [[34.2345, 0], '34'], [[43234.23231450000, 0], '43234'],
+        [[34.5345, 0], '34'], [[43234.73231450000, 0], '43234'],
         [[Decimal('34'), 1], '34'], [[Decimal('34'), 5], '34'],
         [[Decimal('34.0'), 1], '34'], [[Decimal('34.0'), 5], '34'],
-        [[Decimal('34.2345'), 0], '34'], [[Decimal('43234.23231450000'), 0], '43234']
+        [[Decimal('34.2345'), 0], '34'], [[Decimal('43234.23231450000'), 0], '43234'],
+
+        [[1.0], '1'], [['1.0'], '1'], [['-1.0'], '-1'],
+        [[34.2345], '34.2345'], [[43234.23231450000], '43234.2323145'],
+        [[Decimal('34.2345')], '34.2345'], [[Decimal('43234.23231450000')], '43234.2323145'],
+
+        [['34.2345', 2], '34.23'],
+        [['34.2', 1], '34.2'],
+        [['34', 1], '34'], [[34, 5], '34'],
+        [['34.0', 1], '34'], [[34.0, 5], '34'],
+        [['34.2345', 0], '34'], [['43234.23231450000', 0], '43234'],
+        [['34.5345', 0], '34'], [['43234.73231450000', 0], '43234'],
+
+        [['34,2345', 2], '34,23'],
+        [['34,2', 1], '34,2'],
+        [['34,', 1], '34'], [[34, 5], '34'],
+        [['34,0', 1], '34'], [[34.0, 5], '34'],
+        [['34,2345', 0], '34'], [['43234,23231450000', 0], '43234'],
+        [['34,5345', 0], '34'], [['43234,73231450000', 0], '43234'],
+
+        [['34,2345.7', 2], None],
+        [['34,.2', 1], None],
     ]:
         result = nr_normalisation(*test[0])
         assert result == test[1] ,\
             f"invalid function return for" \
             f"\n\tinput: {[type(_) for _ in test[0]], test[0]}" \
-            f"\n\toutput: {result}" \
-            f"\n\texpected: {test[1]}"
-
-    for test in [
-        [1.0, '1'], ['1.0', '1'], ['-1.0', '-1'],
-        [34.2345, '34.2345'], [43234.23231450000, '43234.2323145'],
-        [Decimal('34.2345'), '34.2345'], [Decimal('43234.23231450000'), '43234.2323145'],
-    ]:
-        result = nr_normalisation(test[0])
-        assert result == test[1] ,\
-            f"invalid function return for" \
-            f"\n\tinput: {type(test[0]), test[0]}" \
             f"\n\toutput: {result}" \
             f"\n\texpected: {test[1]}"
 
