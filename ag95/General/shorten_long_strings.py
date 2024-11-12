cfa4a41969aa41de2160c07b295617d0ac7accde
ageorge95 <arteni.george.daniel@gmail.com>
2024-11-12 11:50:09 +0200

    updated the logic from shorten_long_strings.py - the current logic was wrong

--- ag95/General/shorten_long_strings.py
+++ ag95/General/shorten_long_strings.py
@@ -4,15 +4,17 @@ def shorten_long_str(string: AnyStr,
                      separator: AnyStr = '...',
                      cut_length: int = 50):
     if len(string) > cut_length:
-        return f"{string[:cut_length]}{separator}{string[-cut_length:]}"
+        to_cut = int(cut_length/2)
+        return f"{string[:to_cut]}{separator}{string[-to_cut:]}"
     else:
         return string
 
 if __name__ == '__main__':
     for test in [
         [['my_long_string'], 'my_long_string'],
-        [['my_long_string', '...', 4], 'my_l...ring'],
-        [['my_long_string', '<some_random_txt>', 4], 'my_l<some_random_txt>ring']
+        [['my_long_string', '...', 4], 'my...ng'],
+        [['my_long_string', '<some_random_txt>', 4], 'my<some_random_txt>ng'],
+        [['some very long string that I want to cut in half if it exceeds 20 characters', '...', 35], 'some very long st...eds 20 characters']
     ]:
         result = shorten_long_str(*test[0])
         assert result == test[1] ,\
