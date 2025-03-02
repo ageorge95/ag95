4c7c394bc6a29687091ef1969107ee695519e438
ageorge95 <arteni.george.daniel@gmail.com>
2025-03-02 17:32:11 +0200

    added a new installation option "all" which will install all dependencies

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, find_packages
 
-VERSION = '0.3.57'
+VERSION = '0.3.58'
 DESCRIPTION = 'ag95 scripts collection'
 LONG_DESCRIPTION = 'Various scripts that can be used in all kinds of python projects.'
 
@@ -44,7 +44,12 @@ setup(
         "IoT": [
             'requests'
         ],
-        "IO": []
+        "IO": [],
+        "all": ['concurrent_log_handler',
+                'duckdb',
+                'plotly',
+                'keyring',
+                'requests']
     },
     keywords=['python', 'ag95']
 )
