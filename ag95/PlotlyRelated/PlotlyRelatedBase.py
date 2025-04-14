5532ed6d307628a8569032d5f0ee201830469574
ageorge95 <arteni.george.daniel@gmail.com>
2025-04-14 16:40:58 +0300

    rewrote some of the logic from MultiRowPlot (PlotlyRelated backend)

--- ag95/PlotlyRelated/PlotlyRelatedBase.py
+++ ag95/PlotlyRelated/PlotlyRelatedBase.py
@@ -2,9 +2,8 @@ from typing import (List,
                     Dict,
                     Literal)
 import plotly.graph_objects as go
-from datetime import datetime,\
-    timedelta
-from plotly.subplots import make_subplots
+from datetime import (datetime,
+                      timedelta)
 
 class ScatterPlotDef:
     def __init__(self,
@@ -218,19 +217,27 @@ class MultiRowPlot:
                           show_fig: bool = False,
                           include_plotlyjs: bool = False):
 
-        fig = make_subplots(rows=len(self.plots),
-                            cols=1,
-                            shared_xaxes=True,
-                            subplot_titles=[_.title for _ in self.plots],
-                            vertical_spacing = (1/len(self.plots)) * 0.25)
+        layout = dict(
+            hoversubplots="axis",
+            title=dict(text=self.title),
+            hovermode="x",
+            grid=dict(rows=len(self.plots), columns=1),
+        )
 
+        subplots_data = [
+        ]
+
+        # pre-figure creation
+        # create the initial plots
         for row_id, plot in enumerate(self.plots, 1):
-            traces = []
             for i in range(len(plot.x_axis)):
                 kwargs = {'x': plot.x_axis[i],
-                          'y': plot.y_axis[i]}
+                          'y': plot.y_axis[i],
+                          'xaxis': 'x',
+                          'yaxis': f'y{row_id}' if row_id else 'y'}
                 if plot.name:
                     kwargs |= ({'name': plot.name[i]})
+
                 if plot.colors:
                     if plot_type.__name__ is go.Scatter.__name__:
                         kwargs |= ({'line': {'color': plot.colors[i]}})
@@ -244,82 +251,63 @@ class MultiRowPlot:
                     if plot.fill_method:
                         kwargs |= ({'fill': plot.fill_method[i]})
 
-                traces.append(plot_type(**kwargs))
+                subplot = plot_type(**kwargs)
 
-            fig.add_traces(traces,
-                           rows=row_id,
-                           cols=1)
+                subplots_data.append(subplot)
 
+        # create the figure
+        fig = go.Figure(data=subplots_data, layout=layout)
+
+        # post-figure creation
+        # add custom v_rects/ h_rects on top of the created figure
+        for row_id, plot in enumerate(self.plots, 1):
             if plot.v_rects:
                 for _ in plot.v_rects:
-                    fig.add_vrect(**_ | {'row': row_id,
-                                         'col': 1})
+                    fig.add_vrect(**_ | {'xref': 'x',
+                                         'yref': f'y{row_id}'})
+
             if plot.h_rects:
                 for _ in plot.h_rects:
-                    fig.add_hrect(**_ | {'row': row_id,
-                                         'col': 1})
+                    fig.add_hrect(**_ | {'xref': 'x',
+                                         'yref': f'y{row_id}'})
 
+            # if there are any forced limits specified, apply them for the relevant subplots
             if plot.forced_y_limits:
-                fig.update_yaxes(range=[plot.forced_y_limits[0], plot.forced_y_limits[1]],
-                                 row=row_id,
-                                 col=1)
+                fig.update_layout(
+                    {f'yaxis{row_id}': dict(range=[plot.forced_y_limits[0], plot.forced_y_limits[1]])}
+                )
 
             if any([plot.force_show_until_current_datetime, plot.grey_out_missing_data_until_current_datetime]):
-                oldest_datapoint_refined = min([min(_) for _ in plot.x_axis])-timedelta(seconds=5)
-                newest_datapoint_refined = max([max(_) for _ in plot.x_axis])+timedelta(seconds=5)
+                    oldest_datapoint_refined = min([min(_) for _ in plot.x_axis]) - timedelta(seconds=5)
+                    newest_datapoint_refined = max([max(_) for _ in plot.x_axis]) + timedelta(seconds=5)
 
-                if plot.grey_out_missing_data_until_current_datetime and not plot.force_show_until_current_datetime:
-                    # mark the missing data up until the current datetime with grey
-                    fig.add_vrect(x0=max([max(_) for _ in plot.x_axis]),
-                                  x1=plot.now,
-                                  fillcolor='grey',
-                                  opacity=0.5,
-                                  row=row_id,
-                                  col=1)
+                    if plot.grey_out_missing_data_until_current_datetime and not plot.force_show_until_current_datetime:
+                        # mark the missing data up until the current datetime with grey
+                        fig.add_vrect(x0=max([max(_) for _ in plot.x_axis]),
+                                      x1=plot.now,
+                                      fillcolor='grey',
+                                      opacity=0.5,
+                                      xref = 'x',
+                                      yref = f'y{row_id}')
 
-                    # limit the plot horizontally, to see the missing data until the current time
-                    fig.update_xaxes(range=[oldest_datapoint_refined, newest_datapoint_refined])
+                        # limit the plot horizontally, to see the missing data until the current time
+                        fig.update_xaxes(range=[oldest_datapoint_refined, newest_datapoint_refined])
 
-                if plot.force_show_until_current_datetime and not plot.grey_out_missing_data_until_current_datetime:
-                    # force show up until the current datetime
-                    fig.update_xaxes(range=[oldest_datapoint_refined, plot.now])
+                    if plot.force_show_until_current_datetime and not plot.grey_out_missing_data_until_current_datetime:
+                        # force show up until the current datetime
+                        fig.update_xaxes(range=[oldest_datapoint_refined, plot.now])
 
-                if plot.force_show_until_current_datetime and plot.grey_out_missing_data_until_current_datetime:
-                    # force show up until the current datetime
-                    fig.update_layout(xaxis_range=[oldest_datapoint_refined, plot.now])
+                    if plot.force_show_until_current_datetime and plot.grey_out_missing_data_until_current_datetime:
+                        # force show up until the current datetime
+                        fig.update_layout(xaxis_range=[oldest_datapoint_refined, plot.now])
 
-                    # mark the missing data up until the current datetime with grey
-                    fig.add_vrect(x0=max([max(_) for _ in plot.x_axis]),
-                                  x1=plot.now,
-                                  fillcolor='grey',
-                                  opacity=0.5,
-                                  row=row_id,
-                                  col=1)
-
-        update_args = {'title_text': self.title,
-                       'showlegend': self.showlegend}
-
-        # update the height if requested
-        if self.height:
-            update_args |= {'height': self.height}
-
-        # force the tick labels by default
-        update_args |= {'xaxis_showticklabels': True}
-
-        # update data on hover by default
-        update_args |= {'hovermode': 'x'}
-
-        # remove the excessive white margins
-        update_args |= {'margin': dict(l=0, r=0, t=25, b=25)} if not self.title\
-            else {'margin': dict(l=0, r=0, t=42, b=25)}
-
-        # force show the x axis to show on all subplots
-        # xaxis{n}_showticklabels is needed for each axis in order to show the x axis, when the shared_axes is ON
-        if len(self.plots) > 1:
-            force_show_x_axis_args = dict([[f'xaxis{i}_showticklabels', True] for i, _ in enumerate(range(2,len(self.plots)+1),1)])
-            update_args |= force_show_x_axis_args
-
-        fig.update_layout(**update_args)
+                        # mark the missing data up until the current datetime with grey
+                        fig.add_vrect(x0=max([max(_) for _ in plot.x_axis]),
+                                      x1=plot.now,
+                                      fillcolor='grey',
+                                      opacity=0.5,
+                                      xref = 'x',
+                                      yref = f'y{row_id}')
 
         if show_fig:
             fig.show()
