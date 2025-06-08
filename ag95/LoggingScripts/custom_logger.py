0108df06f8307c8fc1fb2b78779077ebe9c2af40
ageorge95 <arteni.george.daniel@gmail.com>
2025-06-08 15:17:55 +0300

    refined custom_logger.py

--- ag95/LoggingScripts/custom_logger.py
+++ ag95/LoggingScripts/custom_logger.py
@@ -8,7 +8,7 @@ from sys import (stdout,
                  platform)
 from os import (system,
                 path,
-                mkdir)
+                makedirs)
 from typing import (Literal,
                     List)
 
@@ -33,11 +33,30 @@ class CustomFormatter(Formatter):
         formatter = Formatter(log_fmt)
         return formatter.format(record)
 
-def configure_logger(log_name : str = "runtime_log.log",
-                     maxBytes: int = 20*1024*1024,
+def _reset_handlers():
+    # Remove all existing handlers from root logger
+    for handler in list(getLogger().handlers):
+        getLogger().removeHandler(handler)
+
+def _check_log_destination(destination_path):
+    # check if the specified log_name can be stored in the provided location
+    destination_path_dirname = path.dirname(destination_path)
+    if destination_path_dirname:
+        if not path.isdir(destination_path_dirname):
+            try:
+                makedirs(destination_path_dirname)
+            except:
+                raise Exception(f'Your log path is invalid: {destination_path}')
+
+def configure_logger(log_name: str = "runtime_log.log",
+                     maxBytes: int = 20 * 1024 * 1024,
                      backupCount: int = 2,
                      log_level: Literal['DEBUG', 'INFO', 'WARNING', 'ERROR', 'CRITICAL'] = DEBUG,
-                     propagate_logger: bool = True):
+                     propagate_logger: bool = True,
+                     allow_handlers_ovewrite_at_runtime: bool = False):
+
+    if allow_handlers_ovewrite_at_runtime:
+        _reset_handlers()
 
     if platform == 'win32':
         # enable colored console text on windows
@@ -52,13 +71,8 @@ def configure_logger(log_name : str = "runtime_log.log",
     ch = StreamHandler(stream=stdout)
     ch.setLevel(DEBUG)
     ch.setFormatter(CustomFormatter())
-    # first check if the log_name specified is actually within a folder and try to create it
-    if (path.dirname(log_name) and
-            not path.isdir(path.dirname(log_name))):
-        try:
-            mkdir(path.dirname(log_name))
-        except:
-            raise Exception(f'Your log name is invalid: {log_name}')
+
+    _check_log_destination(log_name)
 
     # and after that pass the argument along to ConcurrentRotatingFileHandler
     fh = ConcurrentRotatingFileHandler(log_name,
@@ -73,11 +87,15 @@ def configure_logger(log_name : str = "runtime_log.log",
                 level=log_level,
                 handlers=[fh,ch])
 
-def configure_loggers(log_names : List[str] = ("main_logger.log"),
-                      maxBytes: int = 20*1024*1024,
+def configure_loggers(log_names: List[str] = ("main_logger.log"),
+                      maxBytes: int = 20 * 1024 * 1024,
                       backupCount: int = 2,
                       log_level: Literal['DEBUG', 'INFO', 'WARNING', 'ERROR', 'CRITICAL'] = DEBUG,
-                      propagate_logger: bool = True):
+                      propagate_logger: bool = True,
+                      allow_handlers_ovewrite_at_runtime: bool = False):
+
+    if allow_handlers_ovewrite_at_runtime:
+        _reset_handlers()
 
     if platform == 'win32':
         # enable colored console text on windows
@@ -96,13 +114,7 @@ def configure_loggers(log_names : List[str] = ("main_logger.log"),
     # configure each logger in the same manner, but with different file handlers
     for log_name in log_names:
 
-        # first check if the log_name specified is actually within a folder and try to create it
-        if (path.dirname(log_name) and
-                not path.isdir(path.dirname(log_name))):
-            try:
-                mkdir(path.dirname(log_name))
-            except:
-                raise Exception(f'Your log name is invalid: {log_name}')
+        _check_log_destination(log_name)
 
         # and after that pass the argument along to ConcurrentRotatingFileHandler
         fh = ConcurrentRotatingFileHandler(log_name,
@@ -127,10 +139,10 @@ def configure_loggers(log_names : List[str] = ("main_logger.log"),
 
 if __name__ == '__main__':
 
-    # a single root logger
-    configure_logger(log_name='log\\my_log.log',
+    # a single logger no subfolder
+    configure_logger(log_name='my_log.log',
                      log_level='INFO')
-    log = getLogger()
+    log = getLogger('my_log.log')
 
     log.debug('I am a DEBUG message')
     log.info('I am an INFO message')
@@ -138,8 +150,33 @@ if __name__ == '__main__':
     log.error('I am an ERROR message')
     log.critical('I am a CRITICAL message')
 
-    # multiple loggers
-    configure_loggers(log_names=['log\\some_log.log', 'log\\another_log.log'])
+    # a single logger within a subfolder
+    configure_logger(log_name='logs\\my_log_subfolder.log',
+                     log_level='INFO',
+                     allow_handlers_ovewrite_at_runtime=True)
+    log = getLogger('my_log_subfolder.log')
+
+    log.debug('I am a DEBUG message')
+    log.info('I am an INFO message')
+    log.warning('I am a WARNING message')
+    log.error('I am an ERROR message')
+    log.critical('I am a CRITICAL message')
+
+    # a single logger within a nested subfolder
+    configure_logger(log_name='logs\\my_fav_subfolder1\\my_log_nested.log',
+                     log_level='INFO',
+                     allow_handlers_ovewrite_at_runtime=True)
+    log = getLogger('my_log_nested.log')
+
+    log.debug('I am a DEBUG message')
+    log.info('I am an INFO message')
+    log.warning('I am a WARNING message')
+    log.error('I am an ERROR message')
+    log.critical('I am a CRITICAL message')
+
+    # multiple loggers no subfolder
+    configure_loggers(log_names=['some_log.log', 'another_log.log'],
+                      allow_handlers_ovewrite_at_runtime=True)
     log = getLogger('some_log.log')
     log.debug('some_log I am a DEBUG message')
     log.info('some_log I am an INFO message')
@@ -148,6 +185,41 @@ if __name__ == '__main__':
     log.critical('some_log I am a CRITICAL message')
 
     log = getLogger('another_log.log')
+    log.debug('another_log I am a DEBUG message')
+    log.info('another_log I am an INFO message')
+    log.warning('another_log I am a WARNING message')
+    log.error('another_log I am an ERROR message')
+    log.critical('another_log I am a CRITICAL message')
+
+    # multiple loggers within a subfolder
+    configure_loggers(log_names=['logs\\some_log_subfolder.log', 'logs\\another_log_subfolder.log'],
+                     allow_handlers_ovewrite_at_runtime=True)
+    log = getLogger('some_log_subfolder.log')
+    log.debug('some_log I am a DEBUG message')
+    log.info('some_log I am an INFO message')
+    log.warning('some_log I am a WARNING message')
+    log.error('some_log I am an ERROR message')
+    log.critical('some_log I am a CRITICAL message')
+
+    log = getLogger('another_log_subfolder.log')
+    log.debug('another_log I am a DEBUG message')
+    log.info('another_log I am an INFO message')
+    log.warning('another_log I am a WARNING message')
+    log.error('another_log I am an ERROR message')
+    log.critical('another_log I am a CRITICAL message')
+
+    # multiple loggers within a nested subfolder
+    configure_loggers(log_names=['logs\\my_fav_subfolder2\\some_log_nested.log',
+                                 'logs\\my_fav_subfolder2\\another_log_nested.log'],
+                     allow_handlers_ovewrite_at_runtime=True)
+    log = getLogger('some_log_nested.log')
+    log.debug('some_log I am a DEBUG message')
+    log.info('some_log I am an INFO message')
+    log.warning('some_log I am a WARNING message')
+    log.error('some_log I am an ERROR message')
+    log.critical('some_log I am a CRITICAL message')
+
+    log = getLogger('another_log_nested.log')
     log.debug('another_log I am a DEBUG message')
     log.info('another_log I am an INFO message')
     log.warning('another_log I am a WARNING message')
